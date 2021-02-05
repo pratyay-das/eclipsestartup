917acaeb3cd01cca4012de7f33a4b110ae28ca8a
375637 <375637@LTIN214277.cts.com>
2021-02-05 17:51:31 +0530

    Main function is written to run the project

--- Gitdemo/src/practice/pratyay/gitdemo/GitDemo.java
+++ Gitdemo/src/practice/pratyay/gitdemo/GitDemo.java
@@ -11,6 +11,10 @@ public class GitDemo {
 
 	public static void main(String[] args)
 	{
-		
+		GitdemoCalculator gdc = new GitdemoCalculator();
+		int addValue=gdc.add(12, 11);
+		System.out.println("The value after addition is "+ addValue);
+		int subvalue=gdc.sub(12, 11);
+		System.out.println("The value after substraction is "+ subvalue);
 	}
 }
